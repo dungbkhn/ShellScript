b472f4120fcda96871bcc4f60d9455a6f9ab838e
dungbkhn <86149640+dungbkhn@users.noreply.github.com>
2021-07-12 22:59:29 +0700

    Add files via upload

--- comparelistfile_remote.sh
+++ comparelistfile_remote.sh
@@ -117,7 +117,9 @@ if [ -f "$memtemp""/""$param1" ] ; then
 
 	done
 
-
+	#1:same file
+	#0:can hash tung phan de kiem tra ---> copy/append phai hash
+	#5:chi ton tai phia local do phia remote kich thuoc qua lon nen bi xoa --->can copy
 	count=0
 	for i in "${!names[@]}" ; do
 
@@ -128,12 +130,15 @@ if [ -f "$memtemp""/""$param1" ] ; then
 					hassamefile[$i]=1
 					hassamefile_remote[$j]=1
 					if [ "${mtime[$i]}" == "${mtime_remote[$j]}" ] && [ "${filesize[$i]}" == "${filesize_remote[$j]}" ] && [ "${md5hash[$i]}" == "${md5hash_remote[$j]}" ] ; then
-						printf "./%s/1/%s/0/null/0\n" "${names[$i]}" "${names_remote[$j]}" >> "$memtemp""/""$param3"
+						printf "./%s/1/%s/0/null/0/0\n" "${names[$i]}" "${names_remote[$j]}" >> "$memtemp""/""$param3"
 					elif [ "${filesize[$i]}" -lt "${filesize_remote[$j]}" ] ; then
 						rm "$param2""/""${names_remote[$j]}"
-						printf "./%s/5/null/0/null/0\n" "${names[$i]}" >> "$memtemp""/""$param3"
+						printf "./%s/5/null/0/null/0/0\n" "${names[$i]}" >> "$memtemp""/""$param3"
+					elif [ "${md5hash[$i]}" != "${md5hash_remote[$j]}" ] ; then
+						rm "$param2""/""${names_remote[$j]}"
+						printf "./%s/5/null/0/null/0/0\n" "${names[$i]}" >> "$memtemp""/""$param3"
 					else
-						printf "./%s/0/%s/%s/%s/%s\n" "${names[$i]}" "${names_remote[$j]}" "${filesize_remote[$j]}" "${md5hash_remote[$j]}" "${mtime_remote[$j]}" >> "$memtemp""/""$param3"
+						printf "./%s/0/%s/%s/%s/%s/%s\n" "${names[$i]}" "${names_remote[$j]}" "${filesize_remote[$j]}" "${md5hash_remote[$j]}" "${mtime_remote[$j]}" "${mtime[$i]}" >> "$memtemp""/""$param3"
 					fi
 					count=1
 
@@ -182,13 +187,14 @@ if [ -f "$memtemp""/""$param1" ] ; then
 
 	#echo "$count"'------------------------file so sanh------------------------------' >> "$memtemp""/""$param3"
 	#so sanh
+	#4:file chi co o local, ko co o remote ---> can copy
 	for i in "${!names_nt[@]}"
 	do
 		count=0
 		for j in "${!names_remote_nt[@]}"
 		do
 			if [ "${isselected_remote_nt[$j]}" -eq 0 ] && [ "${mtime_nt[$i]}" == "${mtime_remote_nt[$j]}" ] && [ "${filesize_nt[$i]}" == "${filesize_remote_nt[$j]}" ] && [ "${md5hash_nt[$i]}" == "${md5hash_remote_nt[$j]}" ] ; then
-				printf "./%s/2/%s/0/null/0\n" "${names_nt[$i]}" "${names_remote_nt[$j]}" >> "$memtemp""/""$param3"
+				printf "./%s/2/%s/0/null/0/0\n" "${names_nt[$i]}" "${names_remote_nt[$j]}" >> "$memtemp""/""$param3"
 				mv "$param2""/""${names_remote_nt[$j]}" "$param2""/""${names_nt[$i]}"
 				isselected_remote_nt[$j]=1
 				count=1
@@ -198,18 +204,19 @@ if [ -f "$memtemp""/""$param1" ] ; then
 		
 		if [ "$count" -eq 0 ] ; then
 			#echo 'do some thing'
-			printf "./%s/4/null/0/null/0\n" "${names_nt[$i]}" >> "$memtemp""/""$param3"
+			printf "./%s/4/null/0/null/0/0\n" "${names_nt[$i]}" >> "$memtemp""/""$param3"
 		fi
 		
 	done
 
 	#echo '------------------------file bi xoa------------------------------' >> "$memtemp""/""$param3"
 	#xoa nhung file con lai
+	#3:file ko co o local nen bi xoa o remote
 	for i in "${!names_remote_nt[@]}"
 	do
 		if [ "${isselected_remote_nt[$i]}" -eq 0 ] ; then
 			rm "$param2""/""${names_remote_nt[$i]}"
-			printf "./null/3/%s/0/null/0\n" "${names_remote_nt[$j]}" >> "$memtemp""/""$param3"
+			printf "./%s/3/%s/0/null/0\n" "${names_remote_nt[$j]}" "${names_remote_nt[$j]}" >> "$memtemp""/""$param3"
 		fi
 	done
 
