7c4c3de3693e2f1acd8c46f3665e16f035a1c983
dungbkhn <86149640+dungbkhn@users.noreply.github.com>
2021-07-19 23:10:21 +0700

    Add files via upload

--- truncatefile_inremote.sh
+++ truncatefile_inremote.sh
@@ -7,81 +7,66 @@ tempfilename="$1"
 
 filename=$(echo "$2" | tr -d '\n' | xxd -r -p)
 
-waitfilename="/home/backup/.temp/readyfile.being"
+endfilename="/home/backup/.temp/readyfile.ending"
+
+#echo "filename ban dau:""$filename" >> /home/backup/luutru.txt
 
 #neu tham so thu ba = 0
 if [ "$3" -eq 0 ] ; then
 	#neu ton tai tham so thu tu = 0, copy total file --> remove old file
 	if [ "$4" -eq 0 ] ; then
+		
 		rm "$filename"
 		
 		rm "$tempfilename"
-		
-		rm "$waitfilename"
+
+		rm "$endfilename"
 	
 		exit 0
 	#neu la append va file ton tai
 	elif [ -f "$filename" ] ; then
-		#filesize=$(wc -c "$filename" | awk '{print $1}')
+		filesize=$(wc -c "$filename" | awk '{print $1}')
+			
+		truncsize=$(( (filesize / (8*1024*1024) ) * (8*1024*1024) ))
 		
-		#truncsize=$(( (filesize / (8*1024*1024) ) * (8*1024*1024) ))
-
-		#truncate -s "$truncsize" "$filename"
-		
+		truncate -s "$truncsize" "$filename"
+			
 		rm "$tempfilename"
-		
-		rm "$waitfilename"
+
+		rm "$endfilename"
 	
 		exit 0
 	fi
 elif [ "$3" -eq 1 ] ; then
-	if [ -f "$waitfilename" ] ; then
-		cat "$tempfilename" >> "$waitfilename"
+	if [ -f "$filename" ] ; then
+		cat "$tempfilename" >> "$filename"
 	else
-		#if [ -f "$filename" ] ; then
-		#	mv "$filename" "$waitfilename"
-		#	cat "$tempfilename" >> "$waitfilename"
-		#else
-		#	mv "$tempfilename" "$waitfilename"
-		#fi
-		mv "$tempfilename" "$waitfilename"
+		mv "$tempfilename" "$filename"
 	fi
 	
 	rm "$tempfilename"
 	
-	if [ "$4" -eq 0 ] ; then
-		exit 0
-	else
-		filesize=$(wc -c "$waitfilename" | awk '{print $1}')
-		if [ "$filesize" ] && [ "$filesize" -gt 0 ] ; then
-			filesize=$(( $filesize - 1 ))
-			truncate -s "$filesize" "$waitfilename"
-		fi
-		exit 0
-	fi
-	
-#"$3" -ne 0 and 1
-else
-	#mv "$waitfilename" "$filename"
-	
-	if [ ! -f "$filename" ] ; then
-		mv "$waitfilename" "$filename"
-	else
-		filesize=$(wc -c "$filename" | awk '{print $1}')
-		
-		truncsize=$(( (filesize / (8*1024*1024) ) * (8*1024*1024) ))
-
-		truncate -s "$truncsize" "$filename"
-		
-		cat "$waitfilename" >> "$filename"
-		
-		rm "$waitfilename"
-	fi
-	
-	#tinh hash cua file tao ra, so sanh voi hash gui len tu local
-	
 	exit 0
+#"$3" -eq 2
+else
+	if [ "$4" -eq 0 ] ; then
 
+		mv "$filename" "$endfilename"
+
+		#tinh hash cua file tao ra, so sanh voi hash gui len tu local
+		filesize=$(wc -c "$endfilename" | awk '{print $1}')
+		filesize=$(( $filesize - 1 ))
+		truncate -s "$filesize" "$endfilename"
+			
+		exit 0
+		
+	elif [ "$4" -eq 1 ] ; then
+		mv "$endfilename" "$filename"
+		
+		exit 0
+	else
+		exit 1
+	fi
 fi
 
 
