cf115f3581a6dd58f9ce331a0dfec90b0838aa45
dungbkhn <86149640+dungbkhn@users.noreply.github.com>
2021-07-20 12:22:15 +0700

    Add files via upload

--- truncatefile_inremote.sh
+++ truncatefile_inremote.sh
@@ -7,7 +7,7 @@ tempfilename="$1"
 
 filename=$(echo "$2" | tr -d '\n' | xxd -r -p)
 
-endfilename="/home/backup/.temp/readyfile.ending"
+#endfilename="/home/backup/.temp/readyfile.ending"
 
 partialfile="/home/backup/.temp/partialfile.being"
 
@@ -24,8 +24,6 @@ if [ "$3" -eq 0 ] ; then
 		
 		rm "$tempfilename"
 
-		rm "$endfilename"
-		
 		rm "$truncatestatusfile"
 		
 		touch "$truncatestatusfile"
@@ -35,37 +33,51 @@ if [ "$3" -eq 0 ] ; then
 		exit 0
 	#neu la append (ktra file ton tai)
 	elif [ -f "$filename" ] ; then
-		rm "$endfilename"
-		
+
 		rm "$truncatestatusfile"
 		
 		touch "$truncatestatusfile"
 
 		echo "2" >> "$truncatestatusfile"
-		
-		rm "$partialfile"
-		
-		filesize=$(wc -c "$filename" | awk '{print $1}')
-		
-		truncsize=$(( (filesize / (8*1024*1024) ) * (8*1024*1024) ))
-		
-		dd if="$filename" of="$partialfile" bs=10MB count=2 iflag=skip_bytes skip="$truncsize"
-		
-		truncate -s "$truncsize" "$filename"
-		
-		mv "$filename" "$endfilename"
-		
-		echo "$truncsize" >> "$truncatestatusfile"
-		
+
 		rm "$tempfilename"
 
 		exit 0
 	fi
 elif [ "$3" -eq 1 ] ; then
-	if [ -f "$endfilename" ] ; then
-		cat "$tempfilename" >> "$endfilename"
+	if [ -f "$filename" ] ; then
+		count=0
+		while IFS= read -r line ; do
+			if [ "$count" -eq 0 ] ; then
+				appendorcopy="$line"
+			else
+				truncsize="$line"
+			fi
+			count=$(( $count + 1 ))
+		done < "$truncatestatusfile"
+		
+		#neu truncsize rong
+		if [ ! "$truncsize" ] ; then
+			rm "$partialfile"
+		
+			filesize=$(wc -c "$filename" | awk '{print $1}')
+		
+			truncsize=$(( (filesize / (8*1024*1024) ) * (8*1024*1024) ))
+			
+			dd if="$filename" of="$partialfile" bs=10MB count=2 iflag=skip_bytes skip="$truncsize"
+			
+			truncate -s "$truncsize" "$filename"
+			
+			echo "$truncsize" >> "$truncatestatusfile"
+		fi
+		
+		cat "$tempfilename" >> "$filename"
 	else
-		mv "$tempfilename" "$endfilename"
+		mv "$tempfilename" "$filename"
+		
+		truncsize=0
+		
+		echo "$truncsize" >> "$truncatestatusfile"
 	fi
 	
 	rm "$tempfilename"
@@ -75,15 +87,14 @@ elif [ "$3" -eq 1 ] ; then
 else
 	if [ "$4" -eq 0 ] ; then
 
-		filesize=$(wc -c "$endfilename" | awk '{print $1}')
+		filesize=$(wc -c "$filename" | awk '{print $1}')
 		filesize=$(( $filesize - 1 ))
-		truncate -s "$filesize" "$endfilename"
-			
+		truncate -s "$filesize" "$filename"
+
 		exit 0
 		
 	elif [ "$4" -eq 1 ] ; then
-		mv "$endfilename" "$filename"
-		
+		#do nothing
 		exit 0
 	else
 		count=0
@@ -97,9 +108,8 @@ else
 		done < "$truncatestatusfile"
 		
 		if [ "$appendorcopy" -eq 1 ] ; then
-			rm "$endfilename"
+			rm "$filename"
 		else
-			mv "$endfilename" "$filename"
 			truncate -s "$truncsize" "$filename"
 			cat "$partialfile" >> "$filename"
 		fi
